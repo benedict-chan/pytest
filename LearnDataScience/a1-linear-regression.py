0cf067cbd2fed46aa8547cc52b4cc6d0f47d2246
Benedict Chan <purple.enchanted@gmail.com>
2014-11-24 17:07:40 +1100

    Start a testing environment to run scripts from tutorial https://github.com/nborwankar/LearnDataScience

---
+++ LearnDataScience/a1-linear-regression.py
@@ -0,0 +1,8 @@
+# Test scripts from A1 of https://github.com/nborwankar/LearnDataScience
+
+import pandas as pd
+# we have to clean up the raw data set which we will do
+# in the next lesson. But for now let's look at the cleaned up data.
+# import the cleaned up dataset into a pandas data frame
+df = pd.read_csv('../datasets/loanf.csv')
+
