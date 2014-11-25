faa482df7b143da9f87a9985d99df5a88da19adf
Benedict Chan <purple.enchanted@gmail.com>
2014-11-25 17:38:42 +1100

    Added sample data for a2 linear regression, also started a new file to help draft script

---
+++ LearnDataScience/a2-linear-regression.py
@@ -0,0 +1,10 @@
+# Test scripts from A1 of https://github.com/nborwankar/LearnDataScience
+
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+# we have to clean up the raw data set which we will do
+# in the next lesson. But for now let's look at the cleaned up data.
+# import the cleaned up dataset into a pandas data frame
+df = pd.read_csv('../datasets/loansData.csv')
+
