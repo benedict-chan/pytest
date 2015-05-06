af3a114bc20a050e5c7c1503c7b9c3d0ac86c9de
Benedict Chan <purple.enchanted@gmail.com>
2015-05-06 17:27:23 +1000

    start splitting the content out, next to make the record as Dict

--- astrodatatest/test-parse-single.py
+++ astrodatatest/test-parse-single.py
@@ -17,9 +17,23 @@ pattern = "__NOTOC__[\W{}+]"
 re.split(pattern, text)
 content = re.split(pattern, text)[1]
 
-#Use findall here instead of split, because there's a lot of TEXT in between {{  }} XX {{ }}
-re.findall(r"\{\{[^\{]*\}\}", content)
+#Use findall here instead of split, because there's a lot of TEXT that we don't need in between {{  }} XX {{ }}
+template_lines = re.findall(r"\{\{[^\{]*\}\}", content)
 
 
-sline=re.findall(r"\{\{[^\{]*\}\}", content)[0]
-re.split(r"\n\|", sline)
+#Test of spliting each line
+#sline=re.findall(r"\{\{[^\{]*\}\}", content)[0]
+#re.split(r"\n\|", sline)
+
+
+for sline in template_lines:
+	template_split = re.split(r"\n\|", sline)
+	#Fix the data
+	template_split[0] = template_split[0].replace("{{", "") #First line will start with {{
+	template_split[-1] = template_split[-1].replace("\n}}", "") #Last line has \n}} at the end, 
+	#get template name
+	template_name = template_split[0]
+	record_string_list = template_split[1:] #These record string are in the format of ["X1=YY", "X2=ZZ"...]
+	record_list_list = map(lambda x: re.split("=", x) , record_string_list) #These list list is [ ["X1", "YY"], ["X2", ZZ"] , ...]
+	
+
